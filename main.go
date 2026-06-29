8bf5a695f56dd209ea511365d06efe358bf3912f
e4eHeH <raihanalghiffary28@gmail.com>
2026-06-29 14:36:50 +0700

    chore: try decode json request body and encode json response body

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -48,6 +49,66 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Reset the metrics"))
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respError := errorResponse{
+		Error: msg,
+	}
+
+	data, err := json.Marshal(respError)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+
+	type validateMessage struct {
+		Body string `json:"body"`
+	}
+
+	type validResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := validateMessage{}
+	if err := decoder.Decode(&params); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error decoding paarameters")
+		w.WriteHeader(500)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, validResponse{
+		Valid: true,
+	})
+
+}
+
 func main() {
 	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
@@ -70,8 +131,8 @@ func main() {
 	})
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricHandler)
-
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	log.Printf("Server is running on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
